73f6ea320ff042810d6969f3792f2a5f93160adb
valencprado <valenpschool@gmail.com>
2021-12-09 18:13:55 -0300

    Atualização do index

--- src/index.php
+++ src/index.php
@@ -13,31 +13,36 @@
 
    -->
   <meta charset="utf-8">
-  <title> Ayoike</title>
+  <title>Ayoike</title>
   <link href="./src/style.css" rel="stylesheet" type="text/css">
-  <script src='arquivo.js' type='text/javascript'></script>
+  <script src='./src/script.js' type='text/javascript'></script>
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
 
   <!-- Latest compiled JavaScript -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
 
   <link rel="shortcut icon" href="./img/logonavbar.svg" type="image/x-icon">
 </head>
 
 <body>
 
-  <nav id="navbar" class="navbar navbar-expand-sm"> 
+<?php
+include 'conexao.php';
+?>
+  <!-- -->
+  <nav id="navbar" class="navbar navbar-expand-sm">
     <div class="container-fluid">
 
 
-      
+
       <div class=" collapse navbar-collapse">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <a class="navbar-brand" href="#"> <img src="./img/logonavbar.svg" type="image/x-icon"></a></li><!-- logo -->
+          
           <li style="margin-left: 400px;"><a href="#home">Home </a></li>
 
           <li class="mx-2 nav-item"><a href="#ayoike">A AyoiIke</a></li>
@@ -46,14 +51,14 @@
 
         </ul>
 
-        
+
 
         <div class="form-check form-switch" style="margin-top: 2px;;">
-          <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked>
+          <input class="form-check-input " type="checkbox" role="switch" id="flexSwitchCheckChecked" checked>
           <label class="form-check-label" for="flexSwitchCheckChecked"></label>
 
         </div>
-    
+
       </div>
     </div>
 
@@ -61,7 +66,7 @@
     </div>
   </nav>
   <img src="./img/wave1.png" style="width:100%;" id="home">
-  
+  <img src="./img/uniao.svg">
   <p style="text-align: center; width: 744px;">AyoIke é fruto de trabalho em equipe com total responsabilidade, luta, liberdade, criatividade e segurança.
     Com projetos incríveis e missões que impulsionam sucesso e protagonismo!</p>
 
@@ -74,13 +79,13 @@
   <h2 style="text-align: center;" id="projetos">Projetos</h2>
   <br />
   <br />
-   
-<!-- PROJETOS-->
-<div>
-<div class="flamboyant" style="position: fixed; width: 650px; text-align:right;">
+
+  <!-- PROJETOS-->
+  <div>
+    <div class="flamboyant" style="position: fixed; width: 650px; text-align:right;">
       <img src="./img/Flyer Restaurante 2.png">
     </div>
-<article style="margin-left: 45%; margin-bottom:30%;width:440px;">
+    <article style="margin-left: 45%; margin-bottom:30%;width:440px;">
       <h2 style="font-weight: bold;">Restaurante Flamboyant</h2>
       <section>
         <h3>Restaurante</h3>
@@ -102,10 +107,10 @@
         O sistema conta com cadastro e consulta de clientes e produtos, além de delivery e foi desenvolvido em C#.
       </section>
     </article>
-</div>
+  </div>
 
 
-   
+
   <img src="./img/wave2.png" style="width:100%;">
   <br />
   <br />
@@ -113,15 +118,37 @@
   <!-- Contato-->
   <h2>Fale conosco!</h2>
 
-  <div id="contato" style="margin-right:40px">
+  <div id="contato"  class="container-fluid">
+    <div class="row">
+     
+      <form method="post" action="mandarmensagem.php" name="logon">
+     
+				
+        <label for="txtnome">Qual seu nome?</label>
+        <input name="txtnome" type="text" class="form-control input" required id="nome">
+     		
+      
+      
+  
+        <label  for="txtemail">Seu melhor e-mail:</label>
+        <input name="txtemail" type="email" class="form-control" required id="email">
+      
     
+
+    
+        <label for="txtmensagem">Nos conte algo! (Digite sua mensagem)</label>
+        <textarea rows="5" name="txtmensagem" type="text" class="form-control" required id="mensagem"></textarea>
+
+        <button type="submit" class="btn btn-custom">Enviar</button>
+      </form>
+      
+    </div>
   </div>
-  <br />
-  <br />
-  <br />
-  <br />
-  <br />
-  <footer class="container-fluid" style="background-color: #001C55; color:#FFF;"><p style="text-align: right;">@ Copyright - All rights reserved. </p> <br /> <br /> <br /></footer>
+
+ <footer style="background-color: #001C55; color:#FFF;">
+    <h3 style="font-style:italic;">Social</h3>
+    <p style="text-align: right;">@ Copyright - All rights reserved. </p> <br /> <br /> <br />
+  </footer>
 
 
 </body>
