5967643f1f8f91fae7761093cf006c102e54c04d
valencprado <valenpschool@gmail.com>
2021-12-09 18:13:20 -0300

    Corrigir conexão

--- src/conexao.php
+++ src/conexao.php
@@ -5,9 +5,9 @@ $servidor = "localhost";
 // O nome do usuário para se conectar.
 $usuario = "ayoike";
 // A senha doo usuário acima para se conectar.
-$senha = "forcabrilhante";
+$senha = "Figure.09";
 // O nome do seu banco de dados.
-$banco = "db_satyoike";
+$banco = "db_stayoike";
 $cn = new PDO("mysql:host=$servidor;dbname=$banco",
 $usuario, $senha);
 ?>
