e237a9443f61a6db35160be9d46ead1abb11d725
valencprado <valenpschool@gmail.com>
2021-12-10 00:09:41 -0300

    Alteração no banco de dados

--- sql/db_stayoike.sql
+++ sql/db_stayoike.sql
@@ -7,5 +7,7 @@ nome_vis varchar (50) not null,
 emai_vis varchar (100) not null,
 mensagem_vis varchar (500) not null
 );
-CREATE USER 'ayoike@localhost' IDENTIFIED WITH mysql_native_password BY 'forcabrilhante';
-GRANT ALL PRIVILEGES ON db_stayoike.* TO 'ayoike@localhost' WITH GRANT OPTION;
+CREATE USER 'ayoike'@'localhost' IDENTIFIED WITH mysql_native_password BY 'forcabrilhante';
+GRANT ALL PRIVILEGES ON db_stayoike.* TO 'ayoike'@'localhost' WITH GRANT OPTION;
+
+select * from txt_visitante;
